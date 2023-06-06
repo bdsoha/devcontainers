0fda80b38d7af3844d76f0f96db70c33b03edc9b
Dov Benyomin Sohacheski <b@kloud.email>
2023-06-06 11:53:08 +0300

    ✨ Allow multiple certificates (#3)

---
+++ test/custom-root-ca/multiple.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+source dev-container-features-test-lib
+
+check "Cert 1 installed" bash -c "ls /usr/local/share/ca-certificates/custom.sub.crt"
+check "Cert 2 installed" bash -c "ls /usr/local/share/ca-certificates/custom-1.sub.crt"
+
+check "Cert 1 linked" bash -c "ls -alh /etc/ssl/certs | grep custom.sub.crt"
+check "Cert 2 linked" bash -c "ls -alh /etc/ssl/certs | grep custom-1.sub.crt"
+
+reportResults
